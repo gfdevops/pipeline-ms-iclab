60c0bf14475ccbad642fba4138bc88f8cfdbebd2
Gerardo Felmer <desarrollo@satelnet.cl>
2021-01-17 00:49:37 -0300

    test 18

--- vars/git.groovy
+++ vars/git.groovy
@@ -9,6 +9,7 @@ def merge(branchfrom, branchto) {
     def merge_text = 'Merge '+branchfrom+' into '+branchto
     echo merge_text
 
+    sh 'git branch -d '+branchfrom+ ' 2>/dev/null'
     sh 'git checkout -b '+branchfrom+' remotes/origin/'+branchfrom
     sh 'git pull origin '+branchfrom
     sh 'git checkout '+branchto
