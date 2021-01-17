2c5c73c81e99f6765f68cf98479a43bc7c51b2c5
Gerardo Felmer <desarrollo@satelnet.cl>
2021-01-17 01:34:05 -0300

    test 25

--- vars/git.groovy
+++ vars/git.groovy
@@ -9,13 +9,13 @@ def merge(branchfrom, branchto) {
     def merge_text = 'Merge '+branchfrom+' into '+branchto
     echo merge_text
 
-    sh 'git checkout '+branchfrom
+    sh 'git checkout '+branchfrom 
     sh 'git pull origin '+branchfrom
     sh 'git checkout '+branchto
     sh 'git pull origin '+branchto
     sh 'git merge '+branchfrom
-    sh 'git commit -am \" Merge '+branchfrom+' a '+branchto+' \"'
-    sh 'git push origin '+branchto
+    sh 'git commit -am \" Merge '+branchfrom+' a '+branchto+' \"' + '|| true'
+    sh 'git push origin '+branchto 
 }
 
 return this
