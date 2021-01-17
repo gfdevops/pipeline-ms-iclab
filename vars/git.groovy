4a74ec80d88de1061083fd39af55c7103f7758c8
Gerardo Felmer <desarrollo@satelnet.cl>
2021-01-16 22:38:03 -0300

    test 7

--- vars/git.groovy
+++ vars/git.groovy
@@ -6,12 +6,34 @@ def diff(){
 def merge(branchfrom, branchto) {
     figlet 'git merge'
     def merge_text = 'Merge '+branchfrom+' into '+branchto
-
+    echo merge_text
     sh '''
-        git fetch -p
-        git checkout '''+ branchfrom + ''';git pull
-        git merge '''+ branchfrom +''' +''' ''' +'''+ branchto +'''
+        git pull origin main
+        git checkout '''+branchto+'''; git pull origin  '''+branchto+'''
+        git merge '''+branchfrom+'''
         git commit -am '''Merged '''+ branchfrom +''' branch to '''+ branchto +''''
         git push origin ''' + branchto + '''
-        '''
+    '''
 }
+
+return this
+
+
+/*
+
+codigo profe
+
+    git fetch -p
+	git checkout '''+ Origin + ''';git pull
+	
+	git merge '''+ Origin +''' +''' ''' +'''+ Dest +'''
+	
+	git commit -am '''Merged '''+ Origin +''' branch to '''+ Dest +''''
+	
+	git push origin ''' + Dest + '''
+	
+	'''
+
+
+
+*/
