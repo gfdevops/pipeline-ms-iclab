1801483329f904878c846cfb360ebe64a7992b65
Gerardo Felmer <desarrollo@satelnet.cl>
2021-01-16 23:33:31 -0300

    test 8

--- vars/git.groovy
+++ vars/git.groovy
@@ -3,37 +3,17 @@ def diff(){
     sh 'git diff origin/main..' + env.GIT_BRANCH
 }
 
+//branchfrom = realease -> branchto = master
 def merge(branchfrom, branchto) {
     figlet 'git merge'
     def merge_text = 'Merge '+branchfrom+' into '+branchto
     echo merge_text
-    sh '''
-        git pull origin main
-        git checkout '''+branchto+'''; git pull origin  '''+branchto+'''
-        git merge '''+branchfrom+'''
-        git commit -am '''Merged '''+ branchfrom +''' branch to '''+ branchto +''''
-        git push origin ''' + branchto + '''
-    '''
+
+    sh 'git pull'
+    sh 'git checkout '+branchto
+    sh 'git merge'+branchfrom
+    sh 'git commit -am \" Merge '+branchfrom+' a '+branchto+' \"'
+    sh 'git push origin '+branchto
 }
 
 return this
-
-
-/*
-
-codigo profe
-
-    git fetch -p
-	git checkout '''+ Origin + ''';git pull
-	
-	git merge '''+ Origin +''' +''' ''' +'''+ Dest +'''
-	
-	git commit -am '''Merged '''+ Origin +''' branch to '''+ Dest +''''
-	
-	git push origin ''' + Dest + '''
-	
-	'''
-
-
-
-*/
